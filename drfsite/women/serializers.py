1496d8f1ef1dbd92858b6ce17489d504846e4cf7
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-11-25 05:31:19 +0300

    Пишу свой сериализатор Создал класс WomenModel Создал класс WomenSerializer Создал функцию encode

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+def encode():
+    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+    model_sr = WomenSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
