ac1ac187af9bf65f706be17a0dec967ded9ef9b8
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-11-27 19:45:52 +0300

    Написал свой роутер

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -19,7 +19,26 @@ from women.views import WomenViewSet
 from rest_framework import routers
 
 
-router = routers.DefaultRouter()
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        ),
+
+    ]
+
+router = MyCustomRouter()
 router.register(r'women', WomenViewSet, basename='men')
 print(router.urls)
 
