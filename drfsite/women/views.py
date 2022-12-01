4fd6430e92577b9665f194cc7ce89e75f7869fa6
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-12-01 22:59:12 +0300

    Переопределил атрибут user в классе WomenSerializer(чтобы при post запросе, экей постинге записи, пользователь не мог записать изменения в таблицу под именем другого пользователя) Создал 3 новых класса в views.py для показа, изменения и удаления Создал urls для них

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -5,40 +5,23 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Women, Category
 from .serializers import WomenSerializer
 
 
-class WomenViewSet(mixins.CreateModelMixin,
-                   mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
-    # queryset = Women.objects.all()
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-        if not pk:
-            return Women.objects.all()[:3]
-        return Women.objects.filter(pk=pk)
 
-    @action(methods=['get'], detail=True)
-    def category(self, request, pk=None):
-        categories = Category.objects.get(pk=pk)
-        return Response({'categories': categories.name})
-
-
-# class WomenAPIList(generics.ListCreateAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-#
-#
-# class WomenAPIUpdate(generics.UpdateAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-#
-# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
